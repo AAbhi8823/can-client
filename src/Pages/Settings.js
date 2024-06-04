325f23bff83510d8913791cda90dffb77c79eaad
Ankur Singh <ankur.travelomedia@gmail.com>
2024-06-04 18:34:45 +0530

    password reseat is done

--- src/Pages/Settings.js
+++ src/Pages/Settings.js
@@ -3,7 +3,6 @@ import Page from "../Layouts/Pages";
 import { AiOutlineBell } from "react-icons/ai";
 import { BsPersonSlash } from "react-icons/bs";
 import { VscKey } from "react-icons/vsc";
-import ProfileLock from "./ProfileLock";
 import SetPassword from "../Components/SetPassword";
 import BlockedAccounts from "./BlockedAccounts";
 import Notification from "./Notification";
@@ -28,7 +27,6 @@ const Settings = () => {
             </div>
             <div className="flex lg:md:flex-row flex-col items-start justify-center h-calc(100% -[50px]) w-full lg:md:mx-0 px-2 gap-10">
               <div className=" flex-col flex w-[100%]">
-                {/* left side settings */}
                 <div className="flex flex-col w-[100%] flex-wrap">
                   <div
                     className="flex flex-col bg-white rounded-[25px] border-[0.5px] border-[#dfdede] cursor-pointer"
@@ -36,8 +34,6 @@ const Settings = () => {
                       boxShadow: "0px 10px 30px 0px rgba(0, 0, 0, 0.05)",
                     }}
                   >
-                    {/* notification */}
-
                     <div
                       className={`flex flex-row gap-3 pb-4 pt-6 px-8 rounded-t-[25px] ${
                         selectedTab === "notification" ? "bg-[#C31A7F]/25" : ""
@@ -56,7 +52,6 @@ const Settings = () => {
                       </div>
                     </div>
                     <hr />
-                    {/* blocked accounts */}
                     <div
                       className={`flex flex-row gap-3 py-4 px-8 ${
                         selectedTab === "blockedAccounts"
@@ -79,7 +74,6 @@ const Settings = () => {
                       </div>
                     </div>
                     <hr />
-                    {/* Change your Password */}
                     <div
                       className={`flex flex-row gap-3 py-4 px-8 ${
                         selectedTab === "changePassword"
@@ -101,42 +95,12 @@ const Settings = () => {
                       </div>
                     </div>
                     <hr />
-                    {/* change your pin */}
-                    {/* <div
-                      className={`flex flex-row gap-3 pb-6 pt-4 px-8 rounded-b-[25px] ${
-                        selectedTab === "changePin" ? "bg-[#C31A7F]/50" : ""
-                      }`}
-                      onClick={() => handleChangeTab("changePin")}
-                    >
-                      <div className="flex pt-[4px]">
-                        <VscKey size={20} className="  text-[#C31A7F]" />
-                      </div>
-                      <div className=" flex flex-col">
-                        <h3 className="font-semibold text-[18px]">
-                          Change your pin
-                        </h3>
-                        <p className="text-[14px] text-[#7E7E7E]">
-                          Change your pin at any time.
-                        </p>
-                      </div>
-                    </div> */}
                   </div>
                 </div>
               </div>
-
-              {/* right side  */}
-
               <div className="flex-col justify-center  items-start flex lg:md:w-[60%] w-full">
-                {/* change password code */}
                 {selectedTab === "changePassword" && <SetPassword />}
-
-                {/* change Pin code */}
-                {selectedTab === "changePin" && <ProfileLock />}
-
-                {/* change blocked accounts */}
                 {selectedTab === "blockedAccounts" && <BlockedAccounts />}
-
-                {/* change Notifications */}
                 {selectedTab === "notification" && <Notification />}
               </div>
             </div>
