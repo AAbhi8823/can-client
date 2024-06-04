b347330b10ddb5d2759f655b6e63d02f1d18ea31
Ankur Singh <ankur.travelomedia@gmail.com>
2024-06-04 18:39:29 +0530

    password chnagess

--- src/Components/SetPassword.js
+++ src/Components/SetPassword.js
@@ -27,6 +27,15 @@ const SetPassword = () => {
     setConfirmNewPass(e.target.value);
     setPasswordsMatch(newPass === e.target.value);
   };
+  
+  const clearCookies = () => {
+    const cookies = document.cookie.split(";");
+    for (let cookie of cookies) {
+      const eqPos = cookie.indexOf("=");
+      const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+      document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/`;
+    }
+  };
 
   const submitPassword = async () => {
     const config = {
