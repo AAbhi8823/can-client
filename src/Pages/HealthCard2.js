5a64e0b029bc8a4c023b7bc5e74270197579c4e8
Ankur Singh <ankur.travelomedia@gmail.com>
2024-06-13 09:31:54 +0530

    added the some pages

--- src/Pages/HealthCard2.js
+++ src/Pages/HealthCard2.js
@@ -12,23 +12,12 @@ import axios from "axios";
 import { baseurl } from "../Api/baseUrl";
 import { FaFileDownload } from "react-icons/fa";
 import jsPDF from "jspdf";
+import Cookie from "js-cookie";
+import apis from "../Api/baseUrl";
 
 const HealthCard2 = ({ healthCardData }) => {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
-  const name = queryParams.get("name");
-  const dob = queryParams.get("dob");
-  const gender = queryParams.get("gender");
-  const blood_group = queryParams.get("blood_group");
-  const height = queryParams.get("height");
-  const weight = queryParams.get("weight");
-  const cancer_type = queryParams.get("cancer_type");
-  const cancer_stage = queryParams.get("cancer_stage");
-  const current_treatment = queryParams.get("current_treatment");
-  const last_treatment = queryParams.get("last_treatment");
-  const presiding_doctor = queryParams.get("presiding_doctor");
-  const hospital_details_primary = queryParams.get("hospital_details_primary");
-  const hospital_details = queryParams.get("hospital_details");
   const emergencyContacts = [];
   for (const key of queryParams.keys()) {
     if (key.startsWith("emergency_name_")) {
@@ -39,7 +28,7 @@ const HealthCard2 = ({ healthCardData }) => {
     }
   }
 
-  const [userHealthCard, setHealthCardData] = useState()
+  const [userHealthCard, setHealthCardData] = useState();
   const fetchHealthData = async () => {
     const token = Cookies.get("token");
     const id = localStorage.getItem("active_user");
@@ -48,8 +37,8 @@ const HealthCard2 = ({ healthCardData }) => {
         `${baseurl}/api/healthcard?token=${token}&id=${id}`
       );
       if (response.data) {
-        setHealthCardData(response.data.data); 
-        console.log(response.data.data)
+        setHealthCardData(response.data.data);
+        console.log(response.data.data);
       } else {
         console.log("No data received from the API");
       }
@@ -59,7 +48,7 @@ const HealthCard2 = ({ healthCardData }) => {
   };
 
   const DOB = new Date(userHealthCard?.date_of_birth);
-  const currentData = new Date()
+  const currentData = new Date();
 
   const yearOfBirth = DOB.getFullYear();
   const currentYear = currentData.getFullYear();
@@ -104,6 +93,72 @@ const HealthCard2 = ({ healthCardData }) => {
     fetchHealthData();
   }, []);
 
+  const base_token = Cookies.get("token");
+  const [helthdata, setHelthdata] = useState(null);
+  const [userData, setUserData] = useState("");
+  const gethelthCard = async () => {
+    const postToken = Cookie.get("token");
+    try {
+      const response = await axios.get(
+        `${baseurl}/healthcard/get-health-card`,
+        {
+          headers: {
+            Authorization: `Bearer ${postToken}`,
+          },
+        }
+      );
+      if (response.data?.resData?.status === true) {
+        setHelthdata(response.data.resData.data[0]);
+        console.log("response::>>", response);
+      } else {
+        console.log("API error");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    gethelthCard();
+    LandingData();
+  }, []);
+
+  const LandingData = async () => {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${base_token}`,
+        "Content-Type": "application/json",
+      },
+    };
+    try {
+      const response = await axios(`${apis.GET_SINGLE_USER}`, config);
+      console.log(
+        "response:response " + JSON.stringify(response?.data?.resData?.data)
+      );
+      setUserData(response?.data?.resData?.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  console.log("value:>>>>", userData);
+
+  const calculateAge=(dateOfBirth)=> {
+    const birthDate = new Date(dateOfBirth);
+    const currentDate = new Date();
+  
+    let age = currentDate.getFullYear() - birthDate.getFullYear();
+    const monthDiff = currentDate.getMonth() - birthDate.getMonth();
+    const dayDiff = currentDate.getDate() - birthDate.getDate();
+  
+    // If the current date is before the birth date in the current year, subtract one year from the age
+    if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+      age--;
+    }
+  
+    return age;
+  }
+  
+  // Example usage:
 
   return (
     <Page
@@ -120,17 +175,11 @@ const HealthCard2 = ({ healthCardData }) => {
 
               <div className="flex flex-row w-full p-2 justify-end">
                 <div className="flex lg:gap-4  gap-2">
-                  <div className="bg-[#c31a7f38]  p-2 rounded-[12px] cursor-pointer flex justify-center items-center w-9 shadow-md">
-                    <img src={share} alt="img" />
-                  </div>
-                  <div className="bg-[#c31a7f38]   text-[#C31A7F] px-3 py-2 rounded-[12px] cursor-pointer flex justify-center shadow-md w-9 items-center">
-                    <BsDownload
-                      className="flex flex-grow"
-                      onClick={generatePDF}
-                    />
-                  </div>
                   <div className="bg-[#c31a7f38]    text-[#C31A7F] px-3 py-2 bold rounded-[12px] cursor-pointer flex justify-center shadow-md  w-9 items-center">
+                    <NavLink to="/HealthCard1">
                     <MdOutlineModeEditOutline className="flex flex-grow" />
+                    </NavLink>
+                   
                   </div>
                 </div>
               </div>
@@ -157,11 +206,11 @@ const HealthCard2 = ({ healthCardData }) => {
                     </div>
                     <div className="flex flex-row gap-4 py-4 px-14 items-center justify-center">
                       <div className="flex flex-col text-[14px] font-500">
-                        <p className="text-[14px] font-[500]">Fighter</p>
+                        <p className="text-[14px] font-[500]">{userData?.user_profile} </p>
                       </div>
                       <div className="color-[#C4C4C4] w-1 flex"> |</div>
                       <div className="flex flex-col flex-nowrap">
-                        <p className="text-[14px] font-[500]">{age} years</p>
+                        <p className="text-[14px] font-[500]">{calculateAge(userData?.date_of_birth)} Year</p>
                       </div>
                     </div>
                   </div>
@@ -170,19 +219,19 @@ const HealthCard2 = ({ healthCardData }) => {
                       <h2 className="lg:text-[16px] text-[14px]     text-[#C31A7F]">
                         Weight
                       </h2>
-                      <p>{userHealthCard?.weight} KGs</p>
+                      <p>{helthdata?.weight} KGs</p>
                     </div>
                     <div className="flex flex-col w-1/3 items-center px-[12px] border-r-2 border-[#C4C4C4]">
                       <h2 className="lg:text-[16px] text-[14px]    text-[#C31A7F]">
                         Height
                       </h2>
-                      <p>{userHealthCard?.height} CMs</p>
+                      <p>{helthdata?.height} CMs</p>
                     </div>
                     <div className="flex flex-col w-1/3 items-center px-[12px]">
                       <h2 className="lg:text-[16px] text-[14px]    text-[#C31A7F]">
                         Blood
                       </h2>
-                      <p>{userHealthCard?.blood_group}</p>
+                      <p>{helthdata?.blood_group}</p>
                     </div>
                   </div>
                 </div>
@@ -193,13 +242,13 @@ const HealthCard2 = ({ healthCardData }) => {
                         <h2 className="lg:text-[16px] text-[14px]    text-[#C31A7F]">
                           Cancer
                         </h2>
-                        <p>{userHealthCard?.cancer_type}</p>
+                        <p>{helthdata?.cancer_type}</p>
                       </div>
                       <div className="flex flex-col px-2 w-1/2 items-center">
                         <h2 className="lg:text-[16px] text-[14px]    text-[#C31A7F]">
                           Stage
                         </h2>
-                        <p>{userHealthCard?.cancer_stage}</p>
+                        <p>{helthdata?.cancer_stage}</p>
                       </div>
                     </div>
                     <hr className="w-full border-[#C4C4C4] my-2" />
@@ -208,13 +257,13 @@ const HealthCard2 = ({ healthCardData }) => {
                         <h2 className="lg:text-[16px] text-[14px] flex    text-[#C31A7F] ">
                           Current Treatment
                         </h2>
-                        <p>{userHealthCard?.current_treatment}</p>
+                        <p>{helthdata?.current_treatment}</p>
                       </div>
                       <div className="flex flex-col w-1/2 items-center px-2">
                         <h2 className="lg:text-[16px] text-[14px]    text-[#C31A7F] ">
                           Last Treatment
                         </h2>
-                        <p>{userHealthCard?.last_treatment}</p>
+                        <p>{helthdata?.last_treatment}</p>
                       </div>
                     </div>
                   </div>
@@ -222,7 +271,7 @@ const HealthCard2 = ({ healthCardData }) => {
                     <h2 className="lg:text-[16px] text-[14px]    text-[#C31A7F] whitespace-nowrap">
                       Emergency Contact
                     </h2>
-                    {emergencyContacts.map((contact, index) => (
+                    {helthdata?.emergency_contact?.map((contact, index) => (
                       <div
                         key={index}
                         className="flex flex-row justify-between"
@@ -241,13 +290,13 @@ const HealthCard2 = ({ healthCardData }) => {
                         <h2 className="lg:text-[16px] text-[14px]   text-[#C31A7F] ">
                           Presiding Doctor
                         </h2>
-                        <p>{userHealthCard?.presiding_doctor}</p>
+                        <p>{helthdata?.presiding_doctor}</p>
                       </div>
                       <div className="flex flex-col items-center text-center w-1/2 px-2">
                         <h2 className="lg:text-[16px] text-[14px]    text-[#C31A7F]">
                           Hospital Details (primary)
                         </h2>
-                        <p>{userHealthCard?.hospital_details_primary}</p>
+                        <p>{helthdata?.hospital_details_primary}</p>
                       </div>
                     </div>
                     <hr className="w-full border-[#C4C4C4] my-4" />
@@ -255,7 +304,7 @@ const HealthCard2 = ({ healthCardData }) => {
                       <h2 className="lg:text-[16px] text-[14px]   text-[#C31A7F] whitespace-nowrap">
                         Hospital Details
                       </h2>
-                      <p>{userHealthCard?.hospital_details}</p>
+                      <p>{helthdata?.hospital_details}</p>
                     </div>
                   </div>
                 </div>
@@ -271,7 +320,7 @@ const HealthCard2 = ({ healthCardData }) => {
                     Aadhar Card
                   </h2>
                   <a
-                    href={userHealthCard?.aadhaarCard}
+                    href={helthdata?.adhaar_card}
                     className="flex items-center gap-2 cursor-pointer"
                     download
                     target="_blank"
@@ -285,7 +334,7 @@ const HealthCard2 = ({ healthCardData }) => {
                     Fit to Fly
                   </h2>
                   <a
-                    href={userHealthCard?.fit_to_fly_Certificate}
+                    href={helthdata?.fit_to_fly_certificate}
                     className="flex items-center gap-2 cursor-pointer"
                     download
                     target="_blank"
@@ -299,7 +348,7 @@ const HealthCard2 = ({ healthCardData }) => {
                     Biopsy Certificate
                   </h2>
                   <a
-                    href={userHealthCard?.biopsy}
+                    href={helthdata?.biopsy_certificate}
                     className="flex items-center gap-2 cursor-pointer"
                     download
                     target="_blank"
@@ -312,10 +361,7 @@ const HealthCard2 = ({ healthCardData }) => {
 
               <div className="flex justify-end px-10">
                 <NavLink to={"/HealthRecord1"}>
-                
-                  
-                    <h2 className="text-[#3F75F3]">All Medical records</h2>
-                  
+                  <h2 className="text-[#3F75F3]">All Medical records</h2>
                 </NavLink>
               </div>
             </div>
